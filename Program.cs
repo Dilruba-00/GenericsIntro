00753260f4322452cb6bad9e9f347bc05a803691
Dilruba-00 <68999015+Dilruba-00@users.noreply.github.com>
2021-01-21 00:18:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Dilruba");
+
+            Console.WriteLine(isimler.Lenght);
+
+            isimler.Add("Furkan");
+
+            Console.WriteLine(isimler.Lenght);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+        }
+    }
+}
